e4992f62f329ad6f2cab6cb794d2704c17d3992a
Jack Kufa <jack@kufa.io>
2023-08-18 19:09:20 -0400

    add merge sort

---
+++ solutions/mergeSort/mergeSort.go
@@ -0,0 +1,40 @@
+package mergeSort
+
+func mergeSort(a []int) []int {
+	if len(a) <= 1 {
+		return a
+	}
+	mid := len(a) / 2
+	left := mergeSort(a[:mid])
+	right := mergeSort(a[mid:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	leftLen := len(left)
+	rightLen := len(right)
+	var (
+		i, j int
+	)
+	a := []int{}
+	for i < leftLen && j < rightLen {
+		if left[i] < right[j] {
+			a = append(a, left[i])
+			i += 1
+		} else {
+			a = append(a, right[j])
+			j += 1
+		}
+	}
+	a = appendToEnd(a, left, i)
+	a = appendToEnd(a, right, j)
+	return a
+}
+
+func appendToEnd(a1, a2 []int, index int) []int {
+	for index < len(a2) {
+		a1 = append(a1, a2[index])
+		index += 1
+	}
+	return a1
+}
---
+++ solutions/mergeSort/mergeSort_test.go
@@ -0,0 +1,27 @@
+package mergeSort
+
+import (
+	"testing"
+)
+
+func TestMergeSort_Common(t *testing.T) {
+	sorted := []int{1, 2, 3, 4, 5}
+	unsorted := []int{5, 4, 3, 2, 1}
+	a := mergeSort(unsorted)
+	for i := 0; i < len(a); i++ {
+		if a[i] != sorted[i] {
+			t.Errorf("MergeSort(%v) = %v; want %v", unsorted, a, sorted)
+		}
+	}
+}
+
+func TestMergeSort_ArraySizeOfOne(t *testing.T) {
+	a := []int{1}
+	sorted := []int{1}
+	a = mergeSort(a)
+	for i := 0; i < len(a); i++ {
+		if a[i] != sorted[i] {
+			t.Errorf("MergeSort(%v) = %v; want %v", a, a, sorted)
+		}
+	}
+}
